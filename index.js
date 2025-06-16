c4a05e0e9903118c3ba00b24c2e31530da8e011a
BRIAN GITHINJI <brian.githinji3@student.moringaschool.com>
2025-06-16 11:04:49 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Your existing code
+const companyName = 'Scuber';
+
+// Add this line:
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Your existing code
+let companyCeo = 'Susan Smith';
